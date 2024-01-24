3072d3f0fe933df047d96a893100a07299f2ff4a
sergeyivanov89 <iv.srgy@yandex.ru>
2024-01-24 12:30:28 +0300

    add styles for App component

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,13 +1,15 @@
 import { Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
 import { Spinner } from "reactstrap";
+import cn from "classnames";
 
 import Navbar from "../Navbar";
 import { routerConfig } from "@/shared/routerConfig";
+import styles from "./styles.module.scss";
 
 const App = () => {
   return (
-    <div className="">
+    <div className={cn(styles.root)}>
       <Navbar />
 
       <Suspense fallback={<Spinner>Loading...</Spinner>}>
